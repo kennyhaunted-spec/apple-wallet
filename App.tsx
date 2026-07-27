b09e477c6a234d0ea4335c7dccdccf9c4c886f75
kennyhaunted-spec <kennyhaunted@gmail.com>
2026-07-27 23:59:08 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet } from 'react-native';
+import AppNavigator from '@/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <AppNavigator />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000000',
+  },
+});
